e8005d4b5b9d661bc1120caded66dbf496a19160
Norman Law <norman.ml.law@gmail.com>
2022-08-28 15:44:09 +0800

    26th commit

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.shop, name="shop"),
     path('<slug:category_slug>/', views.shop, name='products_by_category'),
     path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+
 ] 
