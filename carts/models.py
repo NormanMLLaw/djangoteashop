fdda90284884fb435a2ac676c4545b65f57eff2c
Norman Law <norman.ml.law@gmail.com>
2022-09-03 18:58:37 +0800

    45th commit

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from shop.models import Product
+from shop.models import Product, Variation
 from accounts.models import Account
 
 # Create your models here.
@@ -14,6 +14,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):  
     user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+    variations = models.ManyToManyField(Variation, blank=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
